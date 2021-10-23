a27d84408331617b0ee0900717f9c7f3585517b8
Michael Green <self@michaelgreen.dev>
2021-10-23 15:39:57 -0700

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
+import os.path
+
 from setuptools import setup, find_packages
 
-
-def read_requirements(requirements_file_path):
-    """Return dependencies from a requirements file as a list.
-
-    Read a requirements '.txt' file, where dependencies are separated by a new line.
-    Removes all comments and options for pip, and return as a list of dependencies.
-
-    :return:    requirements
-    :rtype:     list
-    """
-    with open(requirements_file_path, "r") as f:
-        data = f.readlines()
-    data = [i[: i.find("#")] if "#" in i else i for i in data]
-    data = [i.strip() for i in data if i.strip()]
-    data = [i for i in data if not i.startswith("-")]
-    return data
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 
 with open("README.md", "r") as f:
@@ -36,7 +23,7 @@ setup(
     description="a Linux window manager based on Microsoft's FancyZones",
     entry_points={"console_scripts": ["snappy=snappyzones.__main__:main"]},
     include_package_data=True,
-    install_requires=read_requirements("requirements.txt"),
+    install_requires=["python3-xlib>=0.15"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     name="snappyzones",
@@ -44,7 +31,7 @@ setup(
     package_data={"": ["*.json", "*.txt"]},
     package_dir={"": "src"},
     python_requires=">=3.8",
-    tests_require=read_requirements("requirements_testing.txt"),
+    tests_require=["pytest"],
     url="https://github.com/1mikegrn/snappyzones",
-    version="0.0.5",
+    version="0.0.8",
 )
